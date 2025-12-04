ddf7d1ada028bffc4664a0952f008c7f42a29b9b
thiagoperest <thiago_teodoroperes07@hotmail.com>
2025-12-04 12:33:38 -0300

    feat: add função util formatTimeAgo

---
+++ src/utils/formatDate.js
@@ -0,0 +1,22 @@
+export function formatTimeAgo(dateString) {
+  const now = new Date();
+  const date = new Date(dateString);
+  const diffInSeconds = Math.floor((now - date) / 1000);
+
+  if (diffInSeconds < 60) return `${diffInSeconds} segundos atrás`;
+
+  const diffInMinutes = Math.floor(diffInSeconds / 60);
+  if (diffInMinutes < 60) return `${diffInMinutes} minutos atrás`;
+
+  const diffInHours = Math.floor(diffInMinutes / 60);
+  if (diffInHours < 24) return `${diffInHours} horas atrás`;
+
+  const diffInDays = Math.floor(diffInHours / 24);
+  if (diffInDays < 30) return `${diffInDays} dias atrás`;
+
+  const diffInMonths = Math.floor(diffInDays / 30);
+  if (diffInMonths < 12) return `${diffInMonths} meses atrás`;
+
+  const diffInYears = Math.floor(diffInMonths / 12);
+  return `${diffInYears} ${diffInYears === 1 ? "ano" : "anos"} atrás`;
+}
