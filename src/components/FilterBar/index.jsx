feca53df964a1dcefa518518d2dc90daf2e6162e
thiagoperest <thiago_teodoroperes07@hotmail.com>
2025-12-08 10:11:11 -0300

    feat: add componente SortControls

--- src/components/FilterBar/index.jsx
+++ src/components/FilterBar/index.jsx
@@ -1,14 +1,11 @@
 import AddIcon from "@mui/icons-material/Add";
 import SearchIcon from "@mui/icons-material/Search";
+import SortControls from "../SortControls";
 import styles from "./styles.module.css";
 
 export default function FilterBar({onSearch, onSort}) {
   const handleSearchChange = (e) => {
     onSearch(e.target.value);
-  };
-
-  const handleSortChange = (e) => {
-    onSort(e.target.value);
   };
 
   return (
@@ -24,17 +21,7 @@ export default function FilterBar({onSearch, onSort}) {
       </div>
 
       <div className={styles.content}>
-        <select className={styles.select} onChange={handleSortChange}>
-          <option value="">Ordenar por: Nome</option>
-          <option value="name">Ordenar por: Nome</option>
-          <option value="location">Ordenar por: Localização</option>
-          <option value="condition">Ordenar por: Condição</option>
-          <option value="isActive">Ordenar por: Ativo</option>
-          <option value="startDate">Ordenar por: Data Inicial</option>
-          <option value="lastUpdate">Ordenar por: Data Final</option>
-          <option value="description">Ordenar por: Descrição</option>
-          <option value="isFavorite">Ordenar por: Favoritos</option>
-        </select>
+        <SortControls onSort={onSort} />
 
         <button className={styles.buttonContainer}>
           <AddIcon />
